b2a01d0fbb00ca5ec6ed7aac2b028c71013148c2
Sergey Krapivnitskiy <ya.uplink@yandex.ru>
2022-09-17 12:18:26 +0300

    validators

--- app/api/endpoints/meeting_room.py
+++ app/api/endpoints/meeting_room.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter, Depends, HTTPException
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.api.validators import check_meeting_room_exists, check_name_duplicate
 from app.core.db import get_async_session
 from app.crud.meeting_room import meeting_room_crud
-from app.models.meeting_room import MeetingRoom
 from app.schemas.meeting_room import (
     MeetingRoomCreate, MeetingRoomDB, MeetingRoomUpdate
 )
@@ -87,28 +87,3 @@ async def remove_meeting_room(
     )
     meeting_room = await meeting_room_crud.remove(meeting_room, session)
     return meeting_room
-
-
-async def check_meeting_room_exists(
-        meeting_room_id: int,
-        session: AsyncSession,
-) -> MeetingRoom:
-    meeting_room = await meeting_room_crud.get(meeting_room_id, session)
-    if meeting_room is None:
-        raise HTTPException(
-            status_code=404,
-            detail='Переговорка не найдена!'
-        )
-    return meeting_room
-
-
-async def check_name_duplicate(
-        room_name: str,
-        session: AsyncSession,
-) -> None:
-    room_id = await meeting_room_crud.get_room_id_by_name(room_name, session)
-    if room_id is not None:
-        raise HTTPException(
-            status_code=422,
-            detail='Переговорка с таким именем уже существует!',
-        )
---
+++ app/api/validators.py
@@ -0,0 +1,30 @@
+from fastapi import HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.crud.meeting_room import meeting_room_crud
+from app.models.meeting_room import MeetingRoom
+
+
+async def check_name_duplicate(
+        room_name: str,
+        session: AsyncSession,
+) -> None:
+    room_id = await meeting_room_crud.get_room_id_by_name(room_name, session)
+    if room_id is not None:
+        raise HTTPException(
+            status_code=422,
+            detail='Переговорка с таким именем уже существует!',
+        )
+
+
+async def check_meeting_room_exists(
+        meeting_room_id: int,
+        session: AsyncSession,
+) -> MeetingRoom:
+    meeting_room = await meeting_room_crud.get(meeting_room_id, session)
+    if meeting_room is None:
+        raise HTTPException(
+            status_code=404,
+            detail='Переговорка не найдена!'
+        )
+    return meeting_room
